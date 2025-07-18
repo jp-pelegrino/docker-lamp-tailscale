a02bf26616f37fa5487e15f0cdf357eea3a64204
JP-ICTU_WVCHD <system@jp-ictu.dohwv.local>
2025-07-18 21:53:39 +0800

    modified:   config/phpmyadmin/config.user.inc.php

--- config/phpmyadmin/config.user.inc.php
+++ config/phpmyadmin/config.user.inc.php
@@ -13,41 +13,24 @@ $cfg['Servers'][$i]['host'] = 'db';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
-/* Cookie settings for reverse proxy */
-$cfg['CookieHttpOnly'] = true;
-$cfg['CookieSameSite'] = 'Lax';
-$cfg['LoginCookieValidity'] = 28800; // 8 hours
-
-/* Session settings for reverse proxy */
-$cfg['SessionSavePath'] = '';
-
 /* Disable version check */
 $cfg['VersionCheck'] = false;
 
-/* Handle reverse proxy setup - must be before any redirects */
-// Force HTTPS detection when behind reverse proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-    $_SERVER['SERVER_PORT'] = '443';
-}
+/* Override protocol detection - simple approach */
+$_SERVER['HTTPS'] = 'on';
+$_SERVER['SERVER_PORT'] = '443';
 
-// Additional HTTPS detection methods
-if (isset($_SERVER['HTTP_X_FORWARDED_PORT']) && $_SERVER['HTTP_X_FORWARDED_PORT'] == '443') {
-    $_SERVER['HTTPS'] = 'on';
-    $_SERVER['SERVER_PORT'] = '443';
-}
+/* Session and cookie settings that work with reverse proxy */
+$cfg['CookieHttpOnly'] = false;  // Allow JavaScript access to cookies
+$cfg['CookieSameSite'] = 'None';  // Allow cross-site cookies
+$cfg['LoginCookieValidity'] = 28800; // 8 hours
 
-// Override REQUEST_SCHEME if needed
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
-    $_SERVER['REQUEST_SCHEME'] = $_SERVER['HTTP_X_FORWARDED_PROTO'];
-}
+/* Force disable SSL checks that might cause issues */
+$cfg['ForceSSL'] = false;
 
-/* Force HTTPS URLs in phpMyAdmin */
-$cfg['ForceSSL'] = true;
+/* Set absolute URI to empty to let phpMyAdmin auto-detect */
+$cfg['PmaAbsoluteUri'] = '';
 
-/* Set the absolute URI for phpMyAdmin to handle reverse proxy properly */
-$cfg['PmaAbsoluteUri'] = 'https://wvdoh.dorper-beta.ts.net/phpmyadmin/';
-
-/* Additional security settings for reverse proxy */
-$cfg['TrustedProxies'] = ['172.19.0.6/32']; // nginx container IP
-$cfg['AllowThirdPartyFraming'] = false;
+/* Disable trusted proxies check */
+$cfg['TrustedProxies'] = [];
+$cfg['AllowThirdPartyFraming'] = true;
